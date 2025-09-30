6bbdd2a4ea2c895b347eb9d3349b81d351cbca3e
Abdifatah-Mohamed <yuusufmoharaage@gmail.com>
2025-09-30 17:37:32 +0300

    commited

--- model/User.js
+++ model/User.js
@@ -1,7 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
 const userSchema = new mongoose.Schema({
-    name : String,
-    age : Number,
-    email : String,
+    name : {
+        type: String,
+        minlength: 3,
+        maxlength:20,
+        require:  true,
+    },
+    age : {
+        type: Number,
+        validate(value){
+            if(value <18){
+                throw new Error(`Age can't be less then 18`);
+            }
+        }
+    },
+    email : {
+        type: String,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error(`Email is not valid`);
+            }
+        }
+    },
     password : String,
 })
 const User = mongoose.model('User',userSchema);

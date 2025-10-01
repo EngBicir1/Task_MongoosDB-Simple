74065888c7de1dd0600f94761637011eb395fd2e
Abdifatah-Mohamed <yuusufmoharaage@gmail.com>
2025-10-01 17:58:57 +0300

    commited

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const express = require('express');
 const mongoose = require('mongoose');
 const colors = require('colors');
 
+const app = express();
+app.use(express.json());
 // Connection
 mongoose.connect('mongodb://127.0.0.1:27017/task')
 .then(()=>console.log('Database is connected'))
@@ -9,9 +12,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/task')
 const User = require('./model/User');
 const Task = require('./model/Task');
 
+app.post('/task', async (req,res) => {
+
+    try {
+
+        console.log(req.body);
+        const task = new Task(req.body);
+        console.log(task);
+        await task.save();
+        return res.status(201).json({ success: true, task});
+        
+    } catch (e) {
+        return res.status(400).json({ success: false, message : e.message });
+    }
+
+
+   
+})
+
+
+
+
+const port = process.env.PORT || 4040;
+
+app.listen(port,()=>{
+    console.log(`server is running at port ${port}`);
+})
+
+
+
+
 /*
     /task Post 
     /task Get
+    /task/:id Get
+    /task/:id PATCH /UPDATE
+    /task/:id DELETE
+
+    /USER Post 
+    /USER Get
+    /USER/:id Get
+    /USER/:id PATCH /UPDATE
+    /USER/:id DELETE
 
 */
 

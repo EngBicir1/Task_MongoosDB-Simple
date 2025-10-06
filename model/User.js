cdc08b23c8440e9cff5525067878eb88005db9ed
Abdifatah-Mohamed <yuusufmoharaage@gmail.com>
2025-10-06 17:50:45 +0300

    commited

--- model/User.js
+++ model/User.js
@@ -44,7 +44,7 @@ userSchema.pre('save',async function(next){
 
     const user = this;
     console.log(user.isModified('password'));
-    if(user.isModified('name','age','email','password')){
+    if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password,8);
     }
     next();
